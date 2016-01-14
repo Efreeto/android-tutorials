57b7a8e84922133fa3190cd72aacdba0a9355aba
Woo Kim <wook@voicebox.com>
2016-01-13 16:12:59 -0800

    Chapter 3 complete

--- app/src/main/java/com/voicebox/bignerdrach/geoquiz/QuizActivity.java
+++ app/src/main/java/com/voicebox/bignerdrach/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.voicebox.bignerdrach.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,6 +12,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -29,8 +33,13 @@ public class QuizActivity extends AppCompatActivity {
     private int mCurrentIndex = 0;
 
     private void updateQuestion() {
-        int question = mQuestionBank[mCurrentIndex].getQuestion();
-        mQuestionTextView.setText(question);
+        Log.d(TAG, "Current question index: " + mCurrentIndex);
+        try {
+            TrueFalse question = mQuestionBank[mCurrentIndex];
+            mQuestionTextView.setText(question.getQuestion());
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            Log.e(TAG, "Index was out of bounds", ex);
+        }
     }
 
     private void checkAnswer(boolean userPressedTrue) {
@@ -51,6 +60,7 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
@@ -82,7 +92,7 @@ public class QuizActivity extends AppCompatActivity {
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mCurrentIndex = (mCurrentIndex - 1 + mQuestionBank.length ) % mQuestionBank.length;
+                mCurrentIndex = (mCurrentIndex - 1 /*+ mQuestionBank.length*/ ) % mQuestionBank.length;
                 updateQuestion();
             }
         });
@@ -96,7 +106,47 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
         updateQuestion();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+        outState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     @Override
